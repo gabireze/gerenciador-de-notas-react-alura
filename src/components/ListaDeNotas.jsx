84dda34c80496018cf9b295a45eb92c9cf0abc0c
Gabriel de Rezende Gonçalves <gabireze@gmail.com>
2021-03-25 18:46:36 -0300

    practicing, using plugins and patterning

--- src/components/ListaDeNotas.jsx
+++ src/components/ListaDeNotas.jsx
@@ -1,40 +1,16 @@
 import { Component } from "react";
+import CardNota from "./CardNota";
 
-export default class ListaDeNotas extends Component {
+class ListaDeNotas extends Component {
   render() {
     return (
-      <section>
-        <ul>
-          <li>
-            <section>
-              <header>
-                <h3>Título</h3>
-              </header>
-              <p>Escreva sua nota</p>
-            </section>
-          </li>
-        </ul>
-        <ul>
-          <li>
-            <section>
-              <header>
-                <h3>Título</h3>
-              </header>
-              <p>Escreva sua nota</p>
-            </section>
-          </li>
-        </ul>
-        <ul>
-          <li>
-            <section>
-              <header>
-                <h3>Título</h3>
-              </header>
-              <p>Escreva sua nota</p>
-            </section>
-          </li>
-        </ul>
-      </section>
+      <ul>
+        <li><CardNota/></li>
+        <li><CardNota/></li>
+        <li><CardNota/></li>
+      </ul>
     );
   }
 }
+
+export default ListaDeNotas;
